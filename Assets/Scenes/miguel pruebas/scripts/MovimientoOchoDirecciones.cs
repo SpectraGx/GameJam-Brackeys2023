d2b66ccbf8961c4947c4ad55b046a3a90f31d3a5
miguel13241 <123994008+miguel13241@users.noreply.github.com>
2023-08-25 23:43:30 -0600

    agagaggag

--- Assets/Scenes/miguel pruebas/scripts/MovimientoOchoDirecciones.cs
+++ Assets/Scenes/miguel pruebas/scripts/MovimientoOchoDirecciones.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 
 public class MovimientoOchoDirecciones : MonoBehaviour
 {
+    Animator animator;
     public float velocidad = 5f; // Velocidad de movimiento
     public int score;
+
+    private string currentState;
+
+    //estados de animacion
+    const string Player_Idel="FullHPPlayerIdle";
+    const string Player_Horizontal="PlayerIdleDerecha";
+    const string Player_1_hit="FirstHitPlayerAnim";
+    const string Player_Idel2="2HPPlayerIdle";
+    const string Player_1hit_Move="PLayerIdelDErechaDaño1";
+    const string Player_2_hit="SecondHitPlayerAnim";
+    const string Player_Idel3="LastHPPlayerIdle";
+    const string Player_2hit_Move="playerIdelDerechadaño2";
        void Start()
     {
+        animator = GetComponent<Animator>();
+        animator.Play("FullHPPlayerIdle");
         score = 0;
+    }
+
+    void ChangeAnimationsState(string newState)
+    {
+        if(currentState == newState) return;
+
+        animator.Play(newState);
+
+        currentState = newState;
     }
 
     private void OnTriggerEnter2D(Collider2D Collision)
@@ -63,5 +87,24 @@ public class MovimientoOchoDirecciones : MonoBehaviour
 
         // Aplicar el desplazamiento al objeto
         transform.Translate(desplazamiento);
+
+        if (movimientoHorizontal<0)
+        {
+            //desplazamiento = -velocidad;
+           transform.localScale = new Vector3(-1f,1f,0f); 
+           ChangeAnimationsState(Player_Horizontal);
+        }
+        else if (movimientoHorizontal>0)
+        {
+            //desplazamiento = velocidad;
+            transform.localScale = new Vector3(1f,1f,0f);
+            ChangeAnimationsState(Player_Horizontal);
+        }
+        else
+        {
+            //desplazamiento = 0f;
+            ChangeAnimationsState(Player_Idel);
+        }
+
     }
 }

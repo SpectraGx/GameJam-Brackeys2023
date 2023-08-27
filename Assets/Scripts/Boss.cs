1efd6b42a6a03d79862b100b1304354c20ec7b89
Fernando Mendoza <fernandogx8647@gmail.com>
2023-08-26 23:59:43 -0600

    Boss

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Boss : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform pj; // Referencia al transform del jugador
+    public float moveSpeed = 2f; // Velocidad de movimiento del enemigo
+
+    public FloatVariable PlayerHP;
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        if (other.gameObject.CompareTag("nave"))
+        {
+            PlayerHP.floatValue--;
+            PlayerHP.floatValue--;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Movimiento horizontal hacia adelante
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        // Seguimiento en el eje Y
+        if (pj.position.y > transform.position.y)
+        {
+            // Se mueve el enemigo hacia arriba
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        else if (pj.position.y < transform.position.y)
+        {
+            // Se mueve el enemigo hacia abajo
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }
     }
 }

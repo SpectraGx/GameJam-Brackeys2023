0bc9774010015b000dc051e4dd01a1c98cc3d16e
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2023-08-26 23:22:48 -0700

    dgfsg

--- Assets/Scenes/miguel pruebas/scripts/MovimientoOchoDirecciones.cs
+++ Assets/Scenes/miguel pruebas/scripts/MovimientoOchoDirecciones.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MovimientoOchoDirecciones : MonoBehaviour
 {
@@ -135,5 +136,11 @@ public class MovimientoOchoDirecciones : MonoBehaviour
 
         }
 
+    }
+
+
+    void OnDeath()
+    {
+        SceneManager.LoadScene("Death");
     }
 }

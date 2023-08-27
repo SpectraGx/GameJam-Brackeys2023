e87a2daa608375c822850fa71472da4bb481bfe8
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2023-08-27 02:34:30 -0700

    uyt87

--- Assets/Scripts/ReloadHP.cs
+++ Assets/Scripts/ReloadHP.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class ReloadHP : MonoBehaviour
 {
-    public FloatVariable HP, Score;
+    public FloatVariable HP, Score, HiScore;
+
+    void Start()
+    {
+        HiScore.floatValue = PlayerPrefs.GetFloat("Hi-Score");
+    }
+
     public void ReloadLives()
     {
+
+
         HP.floatValue = 3;
         Score.floatValue = 0;
     }

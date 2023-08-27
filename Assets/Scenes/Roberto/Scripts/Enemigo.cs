f6a1fc855567b7297b488d8fb3945a1a67e07e55
Fernando Mendoza <fernandogx8647@gmail.com>
2023-08-27 03:02:33 -0600

    Prueba sonido

--- Assets/Scenes/Roberto/Scripts/Enemigo.cs
+++ Assets/Scenes/Roberto/Scripts/Enemigo.cs
@@ -6,11 +6,18 @@ public class Enemigo : MonoBehaviour
 {
     
     public FloatVariable PlayerHP;
+
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip audioCris;
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("nave"))
         {
             PlayerHP.floatValue--;
+            ControllAudio.Instance.EjecutarSound(audioCris);
             ScreenShakeV2.Instance.ShakeCamera(3f, 0.1f);
         }
     }

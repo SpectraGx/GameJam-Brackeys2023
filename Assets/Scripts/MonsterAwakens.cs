fcd2267f37b926ddaecd961e46246a95e39b7e72
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2023-08-27 02:02:53 -0700

    h

--- Assets/Scripts/MonsterAwakens.cs
+++ Assets/Scripts/MonsterAwakens.cs
@@ -29,6 +29,7 @@ public class MonsterAwakens : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+        monsterSource = GetComponent<AudioSource>();
         animator.Play("BaitBoss");
     }
 

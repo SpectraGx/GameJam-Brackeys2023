f894d527a82a9aadc7288a71f250a1552ef528c0
Fernando Mendoza <fernandogx8647@gmail.com>
2023-11-09 21:01:16 -0600

    Fix Inputs

--- Assets/Scripts/Player/MovimientoOchoDirecciones.cs
+++ Assets/Scripts/Player/MovimientoOchoDirecciones.cs
@@ -11,17 +11,11 @@ public class MovimientoOchoDirecciones : MonoBehaviour
     public float velocidad = 5f; // Velocidad de movimiento
 
     [SerializeField] InputSystem inputSystem;
-
     Rigidbody2D rb;
-
-
-
 
     private string currentState;
 
-    //estados de animacion
-
-    
+    // Estados de animación
     const string Player_Idel = "FullHPPlayerIdle";
     const string Player_Horizontal = "PlayerIdleDerecha";
     const string Player_Idel2 = "2HPPlayerIdle";
@@ -30,147 +24,109 @@ public class MovimientoOchoDirecciones : MonoBehaviour
     const string Player_2hit_Move = "playerIdelDerechadaño2";
     const string Player_Death = "PlayerDeath";
 
-
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         animator.Play("FullHPPlayerIdle");
         Score.floatValue = 0;
-
-
-
-
     }
 
     void ChangeAnimationsState(string newState)
     {
         if (currentState == newState) return;
-
         animator.Play(newState);
-
         currentState = newState;
     }
-
 
     void Update()
     {
         Inputs();
         // Obtener la entrada del jugador (teclas de flecha o W,S,A,D)
-
         //inputSystem.xMove = Input.GetAxis("Horizontal");
         //inputSystem.yMove = Input.GetAxis("Vertical");
 
-
-
-
-
         //rb.velocity = new Vector2(inputSystem.xMove * velocidad, inputSystem.yMove * velocidad);
-
-
-
-
 
         // Calcular el desplazamiento en la dirección
         //Vector3 desplazamiento = new Vector3(movimientoHorizontal, movimientoVertical, 0f) * velocidad * Time.deltaTime;
 
         // Aplicar el desplazamiento al objeto
         //transform.Translate(desplazamiento);
-        
-
-
 
         switch (HP.floatValue)
         {
             case 3:
                 {
-
                     if (inputSystem.xMove < 0)
                     {
-
                         transform.localScale = new Vector3(-1f, 1f, 0f);
                         ChangeAnimationsState(Player_Horizontal);
                     }
                     else if (inputSystem.xMove > 0)
                     {
-
                         transform.localScale = new Vector3(1f, 1f, 0f);
                         ChangeAnimationsState(Player_Horizontal);
                     }
                     else
                     {
-
                         ChangeAnimationsState(Player_Idel);
                     }
                     break;
                 }
 
-
             case 2:
                 {
                     if (inputSystem.xMove < 0)
                     {
-
                         transform.localScale = new Vector3(-1f, 1f, 0f);
                         ChangeAnimationsState(Player_1hit_Move);
                     }
                     else if (inputSystem.xMove > 0)
                     {
-
                         transform.localScale = new Vector3(1f, 1f, 0f);
                         ChangeAnimationsState(Player_1hit_Move);
                     }
                     else
                     {
-
                         ChangeAnimationsState(Player_Idel2);
                     }
                     break;
                 }
 
-
             case 1:
                 {
                     if (inputSystem.xMove < 0)
                     {
-
                         transform.localScale = new Vector3(-1f, 1f, 0f);
                         ChangeAnimationsState(Player_2hit_Move);
                     }
                     else if (inputSystem.xMove > 0)
                     {
-
                         transform.localScale = new Vector3(1f, 1f, 0f);
                         ChangeAnimationsState(Player_2hit_Move);
                     }
                     else
                     {
-
                         ChangeAnimationsState(Player_Idel3);
                     }
-
-
                     break;
                 }
 
-
             case 0:
-            {
-                ChangeAnimationsState(Player_Death);
-                break;
-            }
-
-
+                {
+                    ChangeAnimationsState(Player_Death);
+                    break;
+                }
         }
-
     }
 
     void Inputs()
     {
-        rb.velocity = new Vector2(inputSystem.xMove * velocidad, inputSystem.yMove * velocidad);
         inputSystem.xMove = Input.GetAxis("Horizontal");
         inputSystem.yMove = Input.GetAxis("Vertical");
 
+        /*
         if(Input.GetKey(KeyCode.A))
         {
             inputSystem.moveR = true;
@@ -207,16 +163,19 @@ public class MovimientoOchoDirecciones : MonoBehaviour
         {
         inputSystem.moveU = false;
         }
+        */
 
+        inputSystem.moveR = Input.GetKey(KeyCode.A);
+        inputSystem.moveL = Input.GetKey(KeyCode.D);
+        inputSystem.moveD = Input.GetKey(KeyCode.S);
+        inputSystem.moveU = Input.GetKey(KeyCode.W);
 
-
-
-
+        rb.velocity = new Vector2(inputSystem.xMove * velocidad, inputSystem.yMove * velocidad);
     }
 
     void OnDeath()
     {
-        SceneManager.LoadScene("Death");
+        SceneManager.LoadScene("Death"); 
         PlayerPrefs.SetFloat("Hi-Score", Score.floatValue);
     }
 }

47452dbe76c3f0c461d8f80d58ce9dcef34c8499
Fernando Mendoza <fernandogx8647@gmail.com>
2023-11-16 14:31:44 -0600

    MimicCount

---
+++ Assets/Scripts/Enemies/mimic/MimicCount.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MimicCount : MonoBehaviour
+{
+   public int mimicCount;
+   public int mimicsAllowedinLevel;
+}

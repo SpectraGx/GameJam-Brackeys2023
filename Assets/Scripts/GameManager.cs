de74dde1b7f2990a398750a13cd0dba3d5796396
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2023-11-17 04:57:03 -0800

    fwef

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] GameController GM;
     [SerializeField] Transform StartPoint;
-    [SerializeField] GameObject PausePanel;
+
 
     private void Awake()
     {
@@ -21,21 +21,7 @@ public class GameManager : MonoBehaviour
         GM.activeIFrames -= Time.deltaTime * GM.GameTime;
 
 
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            if(GM.isGamePaused)
-            {
-                GM.GameTime = 1;
-                PausePanel.SetActive(false);
-                GM.isGamePaused = false;
-            }
-            else
-            {
-                GM.GameTime = 0;
-                PausePanel.SetActive(true);
-                GM.isGamePaused = true;
-            }
-        }
+        
     }
     public void GamePaused()
     {
